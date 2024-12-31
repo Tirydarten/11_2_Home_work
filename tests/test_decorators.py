6f06884ba6a29572430546deee6f2b4a59a6562a
Artem Volkov <tirydarten@mail.ru>
2024-12-31 14:22:51 +0700

    init commit adding a project to git(main branch)

---
+++ tests/test_decorators.py
@@ -0,0 +1,20 @@
+import pytest
+
+def test_my_function_success():
+    # Тестирование успешного выполнения функции
+    pass
+
+
+def test_my_function_error():
+    # Тестирование обработки ошибки
+    pass
+
+
+def test_log_to_file():
+    # Тестирование логирования в файл
+    pass
+
+
+def test_log_error_to_file(tmpdir):
+    # Тестирование логирования ошибки в файл
+    pass
