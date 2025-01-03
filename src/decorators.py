8c063829b7e9ea6570a60c92d31fc5e60821b236
Artem Volkov <tirydarten@mail.ru>
2025-01-03 15:01:08 +0700

    added docstring to decorator (log)

--- src/decorators.py
+++ src/decorators.py
@@ -1,8 +1,16 @@
 from functools import wraps
-from typing import Any, Callable, Optional, TypeVar
+from typing import Any, Optional
 
 
 def log(filename: Optional[str] = None) -> Any:
+    """Декоратор автоматически логирует начало и конец выполнения функции,
+    ее результаты или возникшие ошибки. Декоратор принимает необязательный аргумент filename.
+     Если filename задан логи будут записываться в файл, иначе в консоль:
+    Пример логирования:
+     Ожидаемый вывод в лог-файл mylog.txt при успешном выполнении: my_function ок
+     Ожидаемый вывод при ошибке: my_function error: тип ошибки.
+      Inputs: (1, 2), {} Где тип ошибки заменяется на текст ошибки."""
+
     def decorator(func: Any) -> Any:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
